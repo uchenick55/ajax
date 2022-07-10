aaccf19231550f7014df78bd33bca9300473a97b
uchenick55 <uchenick55@yandex.ru>
2022-07-10 09:06:00 +0300

    первый запрос ajax

---
+++ index.js
@@ -0,0 +1,26 @@
+var a=5;
+$.ajax("https://repetitora.net/api/JS/Images");
+
+a=8;
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const resultBlock = document.querySelector("#result");
+const clickMeButton = document.querySelector("#click-me");
+clickMeButton.addEventListener("click", makeRequest);
+
+function makeRequest() {
+    resultBlock.innerHTML = "reqult";
+}*/
