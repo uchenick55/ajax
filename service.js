9d64a5da5a998bb5bfb886bb42e76b152197564a
uchenick55 <uchenick55@yandex.ru>
2022-07-16 19:47:54 +0300

    Project 1, Tests for everyone - 003, Промисы, Promise, промис

--- service.js
+++
@@ -1,6 +0,0 @@
-function getImages (pageNumber, successCallBack) {
-    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
-    return promise.then((responce)=>{
-        return (responce.data);
-    })
-}
